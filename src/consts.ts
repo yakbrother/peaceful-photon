abfd75fd2d28684be89908eca94614ea67cb1db3
Tim Eaton <tim@reports-inc.com>
2024-04-01 13:54:32 +0200

    Removing old layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Timothy Eaton - Tech Operations and Design';
+export const SITE_DESCRIPTION = 'I like to find problems and solve them before they exist';
