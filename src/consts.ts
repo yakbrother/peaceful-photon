c7f82e300be26ad847f5ad4d7e2cb39c73aafbf2
Tim Eaton <tim@reports-inc.com>
2024-04-01 16:54:30 +0200

    Tweeks!

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Timothy Eaton - Tech Operations and Design';
-export const SITE_DESCRIPTION = 'I like to find problems and solve them before they exist';
+export const SITE_DESCRIPTION = 'I like to solve problems';
